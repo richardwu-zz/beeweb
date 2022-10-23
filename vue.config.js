6efca86d6fd6ff53d55fea26437f8668d620b7b5
richardwu-zz <chungchuanwu@gmail.com>
2022-10-23 21:36:32 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,3 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/beeweb/' : '/'
-};
-
-module.exports = {
-  chainWebpack: config => {
-    config.module.rules.delete('eslint')
-  }
-}
